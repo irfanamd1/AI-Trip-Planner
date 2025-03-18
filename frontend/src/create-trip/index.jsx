be7227a4bb271ebe15a71c69b548da3455d4eb55
Irfan Ahamed <irfanamd14@gmail.com>
2025-03-18 18:01:45 +0530

    minor change

--- frontend/src/create-trip/index.jsx
+++ frontend/src/create-trip/index.jsx
@@ -206,7 +206,7 @@ const CreateTrip = () => {
         <div className='grid grid-cols-3 gap-5 mt-5'>
           {
             SelectBudgetOptions.map((item, index) => (
-              <div key={ index } onClick={ () => handleInputChange('budget', item.title )} className={`p-4 border rounded-lg hover:shadow-lg cursor-pointer ${ formData?.budget == item.title && 'shadow-lg border-black'}`}>
+              <div key={ index } onClick={ () => handleInputChange('budget', item.title )} className={`p-4 border rounded-lg hover:shadow-lg cursor-pointer ${ formData?.budget == item.title && 'shadow-lg border-gray-500'}`}>
                 <h2 className='text-4xl'>{ item.icon }</h2>
                 <h2 className='font-bold text-lg'>{ item.title }</h2>
                 <h2 className='text-sm text-gray-500'>{ item.desc }</h2>
