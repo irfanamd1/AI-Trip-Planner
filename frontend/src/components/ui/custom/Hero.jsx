78ce3b341f30fcf95cc4d512074634bb46957684
Irfan Ahamed <irfanamd14@gmail.com>
2025-03-18 17:53:18 +0530

    minor changes

--- frontend/src/components/ui/custom/Hero.jsx
+++ frontend/src/components/ui/custom/Hero.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col items-center mx-56 gap-9'>
+    <div className='flex flex-col items-center mx-10 md:mx-56 gap-9'>
       <h1 className='font-extrabold text-[40px] text-center mt-16'>
         <span className='text-[#f56551]'>Discover your Next Adventure with AI:</span> Personalized Iteneraries at your Fingertip</h1>
         <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel curator, creating custom itineraies tailored to your intrests and budget.</p>
         <Link to='/create-trip'>
-            <Button className='cursor-pointer'>Get started for Free</Button>
+            <Button className='cursor-pointer mb-10'>Get started for Free</Button>
         </Link>
     </div>
   )
